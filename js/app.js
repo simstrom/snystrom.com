5a3ad7add2922a96d4764ef24f7a26766e860948
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2022-02-05 16:59:52 +0100

    fixed functionality for collapsed nav

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const nav = document.querySelector('.nav-main');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const bars = document.querySelectorAll('.bar');
+const links = document.querySelector('.nav-links');
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', toggleMenu);
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape') closeMenu();
+});
+links.addEventListener('click', (e) => {
+	if (e.target.tagName.toLowerCase() === 'a') closeMenu();
+});
+
+function toggleMenu() {
 	const visibility = nav.getAttribute('data-visible');
 
 	if (visibility === 'false') {
@@ -12,10 +21,14 @@ navToggle.addEventListener('click', () => {
 			bar.classList.add('active');
 		}
 	} else {
-		nav.setAttribute('data-visible', false);
-		navToggle.setAttribute('aria-expanded', false);
-		for (const bar of bars) {
-			bar.classList.remove('active');
-		}
+		closeMenu();
 	}
-});
+}
+
+function closeMenu() {
+	nav.setAttribute('data-visible', false);
+	navToggle.setAttribute('aria-expanded', false);
+	for (const bar of bars) {
+		bar.classList.remove('active');
+	}
+}
