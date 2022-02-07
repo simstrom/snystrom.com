7c08f9a75a139134d6b29c8b34dd27fbfa9930ba
Simon Nystrom <simstrom@users.noreply.github.com>
2022-02-07 23:23:40 +0100

    added email handler and snackbar for form feedback

---
+++ js/email.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('contact-form');
+const snack = document.getElementById('snackbar');
+
+const displaySnack = () => {
+	snack.classList.add('show');
+	setTimeout(() => {
+		snack.classList.remove('show');
+	}, 3000);
+};
+
+const setSnackState = (res) => {
+	if (!res) {
+		snack.classList.add('error');
+		snack.innerText = 'Error sending message. Please try later!';
+	} else {
+		snack.classList.remove('error');
+		snack.innerText = 'Message sent successfully!';
+		form[1].value = form[2].value = '';
+	}
+};
+
+form.addEventListener('submit', async function (e) {
+	e.preventDefault();
+	this.contact_number.value = (Math.random() * 100000) | 0;
+	// await emailjs
+	// 	.sendForm('service_9u8pleo', 'template_hqa0uaq', this)
+	// 	.then(() => setSnackState(true))
+	// 	.catch(() => setSnackState(false))
+	// 	.then(() => displaySnack());
+});
