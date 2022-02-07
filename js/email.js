c1bebfe60e5a55b6f7c75f9b598c252503bb59ea
Simon Nystrom <simstrom@users.noreply.github.com>
2022-02-07 23:50:22 +0100

    snackbar fully responsive

--- js/email.js
+++ js/email.js
@@ -22,9 +22,9 @@ const setSnackState = (res) => {
 form.addEventListener('submit', async function (e) {
 	e.preventDefault();
 	this.contact_number.value = (Math.random() * 100000) | 0;
-	// await emailjs
-	// 	.sendForm('service_9u8pleo', 'template_hqa0uaq', this)
-	// 	.then(() => setSnackState(true))
-	// 	.catch(() => setSnackState(false))
-	// 	.then(() => displaySnack());
+	await emailjs
+		.sendForm('service_9u8pleo', 'template_hqa0uaq', this)
+		.then(() => setSnackState(true))
+		.catch(() => setSnackState(false))
+		.then(() => displaySnack());
 });
