9891517757e2a3320f84ea601c51dcec58ce59aa
Simon Nystrom <simons.nystrom@gmail.com>
2023-01-29 18:09:24 +0100

    init rebuild with next.js & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./pages/**/*.{js, jsx}', './components/**/*.{js,jsx', './layouts/**/*.{js,jsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
