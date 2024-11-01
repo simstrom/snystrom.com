12abb25be3eb955f377eb7e92f6ff2ce2de9f03c
Simon Nyström <simons.nystrom@gmail.com>
2024-11-01 23:12:08 +0100

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 import Button from '@/components/ui/button';
 import { Metadata } from 'next';
+import Image from 'next/image';
+import moonlight from '../public/images/moonlight.svg';
 
 export const metadata: Metadata = {
 	title: 'Page not found',
@@ -7,17 +9,15 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
 	return (
-		<main className="flex h-[70vh] pt-32 sm:pt-40 flex-col gap-1 items-center justify-center">
-			<span className="font-mono text-xs uppercase tracking-wide text-foreground-secondary">
-				Feeling lost?
-			</span>
-			<h1 className="text-xl mb-2">Page not found</h1>
-			<p className="text-sm leading-7 text-foreground-secondary mb-4 text-center">
-				I could not find what you&apos;re looking for... but it&apos;s alright,
+		<main className="grow flex pt-32 sm:pt-20 flex-col gap-1 items-center justify-center">
+			<Image src={moonlight} alt="Illustration of a camper in the forest" draggable={false} />
+			<h1 className="text-3xl mt-6 mb-2">It seems like you are lost</h1>
+			<p className="text-foreground-secondary mb-4 text-center">
+				You have strayed too far from the path. But don&apos;t worry,
 				<br className="hidden sm:block" />
 				we&apos;ll get you right back on track!
 			</p>
-			<Button size="small" href="/">
+			<Button variant="link" href="/">
 				Take me home
 			</Button>
 		</main>
