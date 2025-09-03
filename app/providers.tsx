8839ff3bd5a96e40a7f0487cb3c04123cc2ea2f5
Simon NYSTRÖM <simons.nystrom@gmail.com>
2025-09-03 22:45:21 +0200

    simplified theme switcher to avoid mounting delay

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider
 			attribute="class"
-			defaultTheme="dark"
+			defaultTheme="system"
+			enableSystem
 			enableColorScheme
 			disableTransitionOnChange
 		>
--- components/ui/themeSwitcher.tsx
+++ components/ui/themeSwitcher.tsx
@@ -1,62 +1,26 @@
-'use client';
-
 import { IconMoon, IconSun } from '@/lib/icons';
 import { cn } from '@/lib/utils';
-import { AnimatePresence, motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
 
 export default function ThemeSwitcher({ className }: { className?: string }) {
-	const [mounted, setMounted] = useState(false);
-	const { setTheme, theme } = useTheme();
-
-	// Avoid hydration mismatch
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+	const { setTheme } = useTheme();
 
 	return (
-		<AnimatePresence mode="wait">
-			{mounted ? (
-				<motion.button
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ duration: 0.2 }}
-					onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-					className={cn('overflow-hidden', className)}
-					aria-label={theme === 'dark' ? 'Change to light mode' : 'Change to dark mode'}
-				>
-					<div className="relative h-[18px] w-[18px] flex items-center justify-center">
-						<motion.div
-							className="absolute"
-							initial={{ y: theme === 'light' ? 0 : 50 }}
-							animate={{ y: theme === 'light' ? 0 : 50, rotate: theme === 'light' ? 0 : 100 }}
-							transition={{
-								type: 'spring',
-								stiffness: 300,
-								damping: 30,
-							}}
-						>
-							<IconSun />
-						</motion.div>
-
-						<motion.div
-							className="absolute"
-							initial={{ y: theme === 'dark' ? 0 : -50 }}
-							animate={{ y: theme === 'dark' ? 0 : -50, rotate: theme === 'dark' ? 0 : -200 }}
-							transition={{
-								type: 'spring',
-								stiffness: 300,
-								damping: 30,
-							}}
-						>
-							<IconMoon />
-						</motion.div>
-					</div>
-				</motion.button>
-			) : (
-				<div className="h-9 w-9"></div>
-			)}
-		</AnimatePresence>
+		<>
+			<button
+				onClick={() => setTheme('light')}
+				className={cn('hidden dark:block', className)}
+				aria-label="Change to light mode"
+			>
+				<IconSun />
+			</button>
+			<button
+				onClick={() => setTheme('dark')}
+				className={cn('dark:hidden', className)}
+				aria-label="Change to dark mode"
+			>
+				<IconMoon />
+			</button>
+		</>
 	);
 }
